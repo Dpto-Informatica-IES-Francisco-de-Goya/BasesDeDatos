82c9db4d6a19c6f39bfa3670a522b62686cfb709
Vicdejuan ASIR <dejuan.victor@gmail.com>
2026-03-24 14:18:56 +0100

    ASIR - clase de hoy

--- Ejemplos/02_Modificacion/00_ASIR_erp.sql
+++ Ejemplos/02_Modificacion/00_ASIR_erp.sql
@@ -95,14 +95,14 @@ select * from productos;
 
 -- 0: VER TIPOS DE DATOS
 -- 0.1: Send to SQL Editor -> Create statment
-CREATE TABLE `productos` (
+/* CREATE TABLE `productos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `precio_sucio` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `precio_oferta` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `categoria_id` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+);*/ 
 
 -- 0.2 
 explain productos;
@@ -162,3 +162,195 @@ update productos set precio_sucio = replace(precio_sucio,'Gratis','0.00');
 commit;
 -- ROLLBACK;
 
+-- Ejercicio 9: Cast de Precios
+explain productos;
+select * from productos;
+START transaction;
+-- SET SQL_SAFE_UPDATES = 0; -- INNECESARIO.
+ALTER TABLE productos
+	CHANGE column precio_sucio precio DECIMAL(10,2);
+explain productos;
+ROLLBACK; -- NO HA FUNCIONADO.
+explain productos;
+-- ¿DISTINGUES DDL DE DML?
+
+-- Ejercicio 10: ERROR: 
+show tables;
+select * from pedidos;
+
+SELECT STR_TO_DATE('3-5-2027','%d-%m-%Y');
+SELECT STR_TO_DATE('3-5-2027','%m-%d-%Y');
+SELECT STR_TO_DATE('3-5-27','%m-%d-%y'); -- 27 es 2027
+SELECT STR_TO_DATE('3-5-70','%m-%d-%y'); -- 1970
+SELECT STR_TO_DATE('3-5-69','%m-%d-%y'); -- 2069
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE pedidos 
+	SET fecha_texto = STR_TO_DATE(fecha_texto,'%d/%m/%Y')
+	/*WHERE fecha_texto LIKE '__/__/____' 
+		OR fecha_texto LIKE '_/_/____'
+        OR fecha_texto LIKE '_/__/____'
+        OR fecha_texto LIKE '__/_/____'; */
+	WHERE fecha_texto LIKE '%/%/____' ; -- este filtro es fundamental.
+UPDATE pedidos 
+	SET fecha_texto = STR_TO_DATE(fecha_texto,'%d-%m-%Y')
+	WHERE fecha_texto LIKE '%-%-____' ; -- este filtro es fundamental.
+UPDATE pedidos 
+	SET fecha_texto = STR_TO_DATE(fecha_texto,'%Y.%m.%d')
+	WHERE fecha_texto LIKE '____.%.%' ; -- este filtro es fundamental.
+SET SQL_SAFE_UPDATES = 1;
+
+-- comprobamos "cutre"
+select * from pedidos;
+-- comprobamos bien
+select count(*) from pedidos where fecha_texto like '____-__-__';
+
+-- ¿Cuantas hay mal?
+SELECT 
+	(select count(*) from pedidos) - 
+	(select count(*) from pedidos where fecha_texto like '____-__-__');
+SELECT * FROM pedidos;
+
+-- CAMBIAR EL TIPO DE DATO:
+ALTER TABLE pedidos
+	CHANGE COLUMN fecha_texto fecha DATE;
+explain pedidos;
+
+
+-- 11) PRODUCTOS HUÉRFANOS
+SELECT 
+    *
+FROM
+    productos;
+select * from categorias;
+
+-- no está protegido por el SAFE_UPDATES.
+-- PERO es una """chapuza""" hacerlo así
+UPDATE productos 
+	SET categoria_id = 4 WHERE id = 4;
+
+
+-- ¿Cómo se hace de forma general?
+-- 1) ¿Qué productos están huérfanos?
+-- trampa: select * from productos where categoria_id > 6;
+
+-- OPCION 1: LEFT JOIN
+SELECT *
+FROM productos p
+	LEFT JOIN categorias c ON p.categoria_id = c.id
+    WHERE c.id IS NULL;
+
+-- OPCION 2:
+SELECT *
+FROM productos
+WHERE categoria_id NOT IN
+			(SELECT 
+				id
+			FROM
+				categorias);
+-- UPDATE
+SET SQL_SAFE_UPDATES = 0;
+UPDATE productos 
+	SET categoria_id = 4
+    WHERE 
+		categoria_id 
+			NOT IN 
+		(SELECT id FROM categorias);
+        
+/* FALLA:
+UPDATE productos 
+	SET categoria_id = 4
+    WHERE 
+		id 
+			IN 
+		(SELECT p.id FROM productos p
+			LEFT JOIN categorias c ON p.categoria_id = c.id
+			WHERE c.id IS NULL);*/
+SET SQL_SAFE_UPDATES = 1;
+
+-- 12) CLIENTES HUÉRFANOS
+-- ES IGUAL QUE EL ANTERIOR
+
+/* 13) Deduplicación de clientes: 
+Elimina duplicados manteniendo el ID más bajo. 
+Importante: Reasigna primero los pedidos de los 
+clientes que vas a borrar para no perder el histórico.*/
+
+-- listado de clientes duplicados
+SELECT 
+    COUNT(id), email
+FROM
+    clientes
+GROUP BY email
+HAVING count(id) > 1;
+
+SELECT 
+    id, email
+FROM
+    clientes
+WHERE
+    email IN (SELECT 
+            email
+        FROM
+            clientes
+        GROUP BY email
+        HAVING COUNT(id) > 1);
+        
+select * from pedidos;
+-- solución trampa
+SET SQL_SAFE_UPDATES = 0;
+UPDATE pedidos
+	SET cliente_id = 3
+		WHERE cliente_id IN (4,5);
+DELETE FROM clientes WHERE id IN (4,5);
+select * from clientes;
+SET SQL_SAFE_UPDATES = 1;
+
+-- solución MALA porque solo funciona si hay un único cliente repetido. Si solo hay 
+-- uno repetido, entonces vale, funciona.
+/*UPDATE pedidos 
+	SET cliente_id = (SELECT 
+							min(id)
+						FROM
+							clientes
+						WHERE
+							email IN (SELECT 
+									email
+								FROM
+									clientes
+								GROUP BY email
+								HAVING COUNT(id) > 1))  
+    WHERE cliente_id IN (SELECT 
+							id
+						FROM
+							clientes
+						WHERE
+							email IN (SELECT 
+									email
+								FROM
+									clientes
+								GROUP BY email
+								HAVING COUNT(id) > 1));*/
+
+-- SOLUCIÓN BUENA BUENÍSIMA
+select * from pedidos;
+select 
+	p.id as pedido_id,
+    p.cliente_id,
+	c1.id as c1_id, 
+    c1.email,
+    c2.id as c2_id,
+    c2.email
+    FROM pedidos p 
+	JOIN clientes c1 ON p.cliente_id = c1.id
+	JOIN clientes c2 ON c1.email = c2.email;
+    
+-- Reasignación de pedidos.
+UPDATE pedidos p 
+	JOIN clientes c1 ON p.cliente_id = c1.id
+	JOIN clientes c2 ON c1.email = c2.email 
+SET p.cliente_id = c2.id WHERE c1.id > c2.id ;
+
+-- Eliminación de duplicados.
+DELETE c1 FROM clientes c1 INNER JOIN clientes c2
+	ON c1.email = c2.email WHERE c1.id > c2.id;
